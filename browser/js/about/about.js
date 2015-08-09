3be97f9e754ee27cd8fda958e3b211ded7139fc3
Angie <angi3yang@gmail.com>
2015-08-09 00:30:25 -0400

    finished home and about page, still need to fix navbar height

--- browser/js/about/about.js
+++ browser/js/about/about.js
@@ -3,15 +3,12 @@ app.config(function ($stateProvider) {
     // Register our *about* state.
     $stateProvider.state('about', {
         url: '/about',
-        controller: 'AboutController',
+        // controller: 'AboutController',
         templateUrl: 'js/about/about.html'
     });
 
 });
 
-app.controller('AboutController', function ($scope, FullstackPics) {
+// app.controller('AboutController', function ($scope) {
 
-    // Images of beautiful Fullstack people.
-    $scope.images = _.shuffle(FullstackPics);
-
-});
+// });
--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -7,11 +7,11 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
         link: function (scope) {
 
             scope.items = [
-                { label: 'Home', state: 'home' },
+                { label: 'Home', state: 'home', auth: false },
+                // { label: 'About', state: 'about' , auth: false},
+                { label: 'Dashboard', state: 'dashboard', auth: true },
                 { label: 'About', state: 'about' },
-                { label: 'Documentation', state: 'docs' },
-                { label: 'Dashboard', state: 'dashboard' },
-                { label: 'Members Only', state: 'membersOnly', auth: true }
+                { label: 'My Progress', state: 'membersOnly', auth: true }
             ];
 
             scope.user = null;
